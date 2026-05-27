ee8971403992546f1e6fe992c09ad5ff6947a754
Denys Dovgan <ytcsgoatlas@gmail.com>
2026-05-28 01:08:06 +0200

    initial structure

--- s30460_2026.py
+++ s30460_2026.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def get_valid_length():
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_valid_id():
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def make_dna(n):
+    pass
+
+
+def stats(seq):
+    pass
+
+
+def embed_name(seq, name):
+    pass
+
+
+def to_fasta(sid, desc, seq, width=80):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
